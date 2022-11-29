19afd0857e51118e9b99fa42102fd04d1a490435
Sable405 <111007485+Sable405@users.noreply.github.com>
2022-11-29 12:33:54 -0800

    Done

--- Prototype 5.1/Assets/Scripts/GameManager.cs
+++ Prototype 5.1/Assets/Scripts/GameManager.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> Target; 
-
-public bool IsGameActive = true;
-public int Score = 0;
-public TextMeshProUGUI ScoreText;
-public TextMeshProUGUI GameOverText;
-public Button RestartButton;
+    public bool IsGameActive;
+    public int Score = 0;
+    public float SpawnRate = 1f;
+    public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI GameOverText;
+    //public TextMeshProUGUI GameTitleText;
+    //public Button StartButton;
+    public GameObject StartScene;
+    public Button RestartButton;
+    public List<GameObject> Target;
 
     // Start is called before the first frame update
     void Start()
     {
-        ScoreText.text= "Score: " + Score;
+        IsGameActive = false;
+    }
+
+    public void StartGame(int difficulty)
+    {
+        //GameTitleText.gameObject.SetActive(false);
+        //StartButton.gameObject.SetActive(false);
+        StartScene.gameObject.SetActive(false);
+        ScoreText.gameObject.SetActive(true);
+        IsGameActive = true;
+        ScoreText.text = "Score: " + Score;
+        SpawnRate /= difficulty;
         StartCoroutine(SpawnTarget());
     }
+
     public void UpdateScore(int addToScore)
     {
         Score += addToScore;
@@ -40,12 +56,12 @@ public Button RestartButton;
 
     IEnumerator SpawnTarget()
     {
-            while(IsGameActive)
-            {
-            yield return new WaitForSeconds(1);
+        while(IsGameActive)
+        {
+            yield return new WaitForSeconds(SpawnRate);
             int index = Random.Range(0, Target.Count);
             Instantiate(Target[index]);
-            }
-            
+        }
+        
     }
 }
